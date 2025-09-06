83c444c723508d78f1acd582925a2a3d283ca00a
AlAminNahid <robin.nahid123@gmail.com>
2025-09-06 17:11:28 +0600

    add a title and a paragraph in index file

---
+++ home_page/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Clinic Management System</title>
+    </head>
+    <body>
+        <p>Clinic Management System is on going</p>
+    </body>
+</html>
