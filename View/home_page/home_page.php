7b5b6510d5caad1a0078157a9be6f005648153b5
AlAminNahid <robin.nahid123@gmail.com>
2025-09-07 09:39:15 +0600

    add nev to the home_page

--- View/home_page/home_page.php
+++ View/home_page/home_page.php
@@ -1,9 +1,19 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Clinic Management System</title>
     </head>
     <body>
-        <p>Clinic Management System is on going</p>
+        <div>Clinic Management System</div>
+        <nav>
+            <ul>
+                <li><a href="">Login</a></li>
+                <li><a href="">Register</a></li>
+                <li><a href="">About</a></li>
+                <li><a href=""></a>Contact</li>
+            </ul>
+        </nav>
     </body>
 </html>
